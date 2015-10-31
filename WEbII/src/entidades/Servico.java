dfa391694c9190f4ef42e689aebc6cda342d1024
vinaud <leyenasd@gmail.com>
2015-10-31 16:04:33 -0300

    Adicionado arquivo persistence.xml (falta configurar direito)

--- WEbII/src/entidades/Servico.java
+++ WEbII/src/entidades/Servico.java
@@ -1,9 +1,16 @@
 package entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Servico {
 
 	
 	private String nome;
+	
+	@Id
+	@GeneratedValue
 	private long id;
 }
