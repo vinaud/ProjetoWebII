61eb24e3124d12ccc7b417e622aa4e61c708deac
vinaud <leyenasd@gmail.com>
2015-11-16 13:16:30 -0300

    Criada entidade que associa central com serviços

---
+++ ProjetoWebServicos/src/entidades/JoinCentralServico.java
@@ -0,0 +1,55 @@
+package entidades;
+
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class JoinCentralServico {
+	
+	/* Classe que faza associação entre uma central de serviço e seus serviços */
+	
+	@Id
+	private long centralID;
+	
+	@Id
+	private long servicoID;
+	
+	@Column
+	private int tempo;
+	
+	@Column
+	private Integer nota;
+
+	public long getCentralID() {
+		return centralID;
+	}
+
+	public void setCentralID(long centralID) {
+		this.centralID = centralID;
+	}
+
+	public long getServicoID() {
+		return servicoID;
+	}
+
+	public void setServicoID(long servicoID) {
+		this.servicoID = servicoID;
+	}
+
+	public int getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+
+	public Integer getNota() {
+		return nota;
+	}
+
+	public void setNota(Integer nota) {
+		this.nota = nota;
+	}
+
+}
